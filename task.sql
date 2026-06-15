689b88722650d4fb1cc18b6fcd890b72f403bf64
Artem <artemkluenkov41@gmail.com>
2026-06-15 17:56:53 +0000

    solution

--- task.sql
+++ task.sql
@@ -2,7 +2,32 @@ DROP DATABASE IF EXISTS ShopDB;
 CREATE DATABASE ShopDB;
 USE ShopDB; 
 
--- Create your stored procedure here
+CREATE TABLE Countries (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Products (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    ProductName VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Warehouses (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    WarehouseName VARCHAR(100) NOT NULL,
+    WarehouseAddress VARCHAR(255) NOT NULL,
+    CountryID INT,
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE SET NULL
+);
+
+CREATE TABLE ProductInventory (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    ProductID INT,
+    WarehouseAmount INT NOT NULL DEFAULT 0,
+    WarehouseID INT,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE CASCADE,
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE CASCADE
+);
 
 DELIMITER //
 
